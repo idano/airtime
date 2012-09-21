1146afc932551bfaa66059c333a8bac104cdd36d
mseelaus <manuel@seelaus.ch>
2012-09-21 15:01:03 -0700

    fix multiple problems with state per station

--- airtime.py
+++ airtime.py
@@ -7,7 +7,9 @@ import datetime
 from collections import defaultdict
 
 
-logfile = "/var/log/messages"
+#logfile = "/var/log/messages"
+#logfile = "emmanuel.messages"
+logfile = "messages"
 
 
 fo = open(logfile)
@@ -23,6 +25,8 @@ macnames['98:0c:82:34:11:eb'] = 'Manuel Inspire'
 macnames['40:fc:89:3c:51:35'] = 'Manuel Atrix'
 macnames['90:84:0d:82:25:3f'] = 'Tracy iPhone'
 macnames['00:23:6c:88:f5:9f'] = 'Tracy Macbook'
+macnames['00:15:af:50:9d:21'] = 'Emmanuel Geri'
+macnames['ec:55:f9:4d:b7:38'] = 'Yvonne'
 
 
 
@@ -48,8 +52,8 @@ if __name__ == "__main__":
     total = datetime.datetime.min
     timeused = datetime.datetime.min
     dailytotals = defaultdict(dict)
-    action = ''
-    
+    state = {}
+
     # filter out the lines concerning association and disassociation of Es
     # computer
     for line in fo:
@@ -57,7 +61,8 @@ if __name__ == "__main__":
         if " Associated with station " in line or " Disassociated with station " in line:
             results.append(line)
 
-    starttime = datetime.datetime.min
+    starttime = {}
+
     for line in results:
         words = string.split(line)
         month = words[0]
@@ -68,25 +73,34 @@ if __name__ == "__main__":
         # if this day has not been initialized, do it
         if macAddr not in dailytotals:
             dailytotals[macAddr] = dict()
+        if macAddr not in state:
+            state[macAddr] = 'disassociated'
+        if macAddr not in starttime:
+            starttime[macAddr] = datetime.datetime.min
         if month+day not in dailytotals[macAddr]:
             dailytotals[macAddr][month+day] = datetime.datetime.min
-        if "Associated" in line:
-            action = "associated"
-            starttime = logtime 
+        if "Associated" in line and state[macAddr] != "associated":
+            state[macAddr] = "associated"
+            starttime[macAddr] = logtime
         elif "Disassociated" in line:
-            action = "disassociated"
-            if starttime != datetime.datetime.min:
-                timeused = logtime - starttime
-                dailytotals[macAddr][month+day] += timeused 
-    if action == "associated":
-        timeused = datetime.datetime.now() - starttime
-        dailytotals[macAddr][month+day] += timeused
+            state[macAddr] = "disassociated"
+            if starttime[macAddr] != datetime.datetime.min:
+                timeused = logtime - starttime[macAddr]
+                # re-initialize starttime[macAddr]
+                dailytotals[macAddr][month+day] += timeused
+                #print macnames[macAddr], month+day, "start: ", starttime[macAddr], "end: ", logtime, "delta: ",unicode(timeused), "total: ", unicode(dailytotals[macAddr][month+day]), state[macAddr]
+                starttime[macAddr] = datetime.datetime.min;
+    # if there's a line with associated at end of file (means the station hasn't currently disassociated yet)...
+    for macAddr in state:
+      if state[macAddr] == "associated":
+          timeused = datetime.datetime.now() - starttime[macAddr]
+          dailytotals[macAddr][month+day] += timeused
 
     for mac in dailytotals:
       try:
         print macnames[mac]+":"
       except:
         print mac+":"
-      for day in dailytotals[mac]:
+      for day in sorted(dailytotals[mac]):
         print day, dailytotals[mac][day]
       print ""
